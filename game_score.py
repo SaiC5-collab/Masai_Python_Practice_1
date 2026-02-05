9d4de6ca1d52f00b7024d9cb8163377571183206
SaiC5-collab <saicharancherupalli@gmail.com>
2026-02-06 00:13:33 +0530

    added assesment python file.

---
+++ game_score.py
@@ -0,0 +1,9 @@
+player_name = input("Enter Player name: ")
+no_of_gamesplayed_byplayer = int(input("Enter no of games played: "))
+total_sore = int(input("Total score achieved by Player: "))
+averagescoreofplayer = total_sore/no_of_gamesplayed_byplayer
+
+print(f"Player: {player_name}")
+print(f"Games Played: {no_of_gamesplayed_byplayer}")
+print(f"Total Score: {total_sore}")
+print(f"Average Score: {averagescoreofplayer}")
